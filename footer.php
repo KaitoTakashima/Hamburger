d640f392545bfe8dd6e55b38c210b50ff09211a1
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-04-18 23:27:35 +0900

    footer.php作成

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="l-footer">
+            <ul class="l-footer_menu">
+                <!-- <li>ショップ情報 | </li>
+                <li>ヒストリー</li> -->
+                <?php wp_nav_menu(); ?>
+            </ul>
+            <address class="copyright">Copyright: RaiseTech</address>
+        </footer>
+    </div>
+
+    <script src="./js/jquery-3.4.1.min.js"></script>
+    <script src="./js/hamburger.js"></script>
+</body>
+
+</html>

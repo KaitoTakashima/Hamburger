bffc8c9787f2135988d35349237573c043336484
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-04-19 10:17:52 +0900

    header,footer分割

--- index.php
+++ index.php
@@ -1,88 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TestPage</title>
-
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <div class="wrapper">
-        <header class="l-header">
-            <div class="l-header_btn">
-                <li class="p-gmenu">
-                    <button class="l-munu_btn c-menu_btn p-hamburger p-gmenu--bt is-hamburger">
-                        <span>Menu</span>
-                    </button>
-                    <ul class="list">
-                        <span>Menu</span>
-                        <li>バーガー</li>
-                        <ul>
-                            <li>ハンバーガー</li>
-                            <li>チーズバーガー</li>
-                            <li>テリヤキバーガー</li>
-                            <li>アボガドバーガー</li>
-                            <li>フィッシュバーガー</li>
-                            <li>ベーコンバーガー</li>
-                            <li>チキンバーガー</li>
-                        </ul>
-                        <li>サイド</li>
-                        <ul>
-                            <li>ポテト</li>
-                            <li>サラダ</li>
-                            <li>ナゲット</li>
-                            <li>コーン</li>
-                        </ul>
-                        <li>ドリンク</li>
-                        <ul>
-                            <li>コーラ</li>
-                            <li>ファンタ</li>
-                            <li>オレンジ</li>
-                            <li>アップル</li>
-                            <li>紅茶(Ice/Hot)</li>
-                            <li>コーヒー(Ice/Hot)</li>
-                        </ul>
-                    </ul>
-                </li>
-            </div>
-            <div class="flex_header">
-                <h1>Hamburger</h1>
-                <form action="#" class="l-search_form p-search_form">
-                    <label for="search" class="screen_reader_text">
-                        <input type="text" class="p-search_form_keyword" name="search" id="search">
-                        <input type="submit" class="p-search_form_submit" value="検索">
-                    </label>
-                </form>
-            </div>
-        </header>
+<?php get_header(); ?>  <!-- header.phpを読み込むテンプレートタグ（インクルードタグ） -->
 
         <article class="contents">
             <section class="main_visual">
                 <h2>ダミーサイト</h2>
             </section>
 
+ 
+
             <div class="branch_menu">
                 <ul class="p-munu">
-                    <li class="p-takeout">
-                        <h2 class="p-title">Take Out</h2>
-                        <ul>
-                            <li class="p-head">
-                                <dl>
-                                    <dt class="p-heading">小見出しが入ります</dt>
-                                    <dd class="p-text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
-                                </dl>
-                            </li>
-                            <li class="p-foot">
-                                <dl>
-                                    <dt class="p-heading">小見出しが入ります</dt>
-                                    <dd class="p-text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
-                                </dl>
-                            </li>
-                        </ul>
-                    </li>
+                   <a href = "<?php echo get_category_link(2); ?>">
+                        <li class="p-takeout">
+                            <h2 class="p-title">Take Out</h2>
+                            <ul>
+                                <li class="p-head">
+                                    <dl>
+                                        <dt class="p-heading">小見出しが入ります</dt>
+                                        <dd class="p-text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                                    </dl>
+                                </li>
+                                <li class="p-foot">
+                                    <dl>
+                                        <dt class="p-heading">小見出しが入ります</dt>
+                                        <dd class="p-text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                                    </dl>
+                                </li>
+                            </ul>
+                        </li>
+                    </a> 
                     <li class="p-eatin">
                         <h2 class="p-title">Eat In</h2>
                         <ul>
@@ -116,17 +61,4 @@
             </div>
         </article>
 
-        <footer class="l-footer">
-            <ul class="l-footer_menu">
-                <li>ショップ情報 | </li>
-                <li>ヒストリー</li>
-            </ul>
-            <address class="copyright">Copyright: RaiseTech</address>
-        </footer>
-    </div>
-
-    <script src="./js/jquery-3.4.1.min.js"></script>
-    <script src="./js/hamburger.js"></script>
-</body>
-
-</html>
+<?php get_footer(); ?>  <!--footer.phpを読み込むテンプレートタグ（インクルードタグ） -->

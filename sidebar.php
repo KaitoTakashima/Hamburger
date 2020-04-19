3710a389bcbd506950759d2b9723776e36f4bed6
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-04-19 11:48:21 +0900

    追加

---
+++ sidebar.php
@@ -0,0 +1,34 @@
+<li class="p-gmenu">
+                    <button class="l-munu_btn c-menu_btn p-hamburger p-gmenu--bt is-hamburger">
+                        <span>Menu</span>
+                    </button>
+                    <ul class="list">
+                        <span>Menu</span>
+                        <li>バーガー</li>
+                        <ul>
+                            <li>ハンバーガー</li>
+                            <li>チーズバーガー</li>
+                            <li>テリヤキバーガー</li>
+                            <li>アボガドバーガー</li>
+                            <li>フィッシュバーガー</li>
+                            <li>ベーコンバーガー</li>
+                            <li>チキンバーガー</li>
+                        </ul>
+                        <li>サイド</li>
+                        <ul>
+                            <li>ポテト</li>
+                            <li>サラダ</li>
+                            <li>ナゲット</li>
+                            <li>コーン</li>
+                        </ul>
+                        <li>ドリンク</li>
+                        <ul>
+                            <li>コーラ</li>
+                            <li>ファンタ</li>
+                            <li>オレンジ</li>
+                            <li>アップル</li>
+                            <li>紅茶(Ice/Hot)</li>
+                            <li>コーヒー(Ice/Hot)</li>
+                        </ul>
+                    </ul>
+                </li>

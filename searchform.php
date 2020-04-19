2ca94276623250d2ede145f7510273f1a06f4c13
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-04-19 11:49:56 +0900

    追加

---
+++ searchform.php
@@ -0,0 +1,7 @@
+
+     <form class="l-search_form p-search_form" id="searchform" method="get" action="<?php echo home_url('/');?>">
+           <label for="search" class="screen_reader_text">
+                <input type="text" class="p-search_form_keyword" name="s" id="s">
+                 <input class="p-search_form_submit" id="searchsubmit" value="検索" type="submit">
+            </label>
+ </form>

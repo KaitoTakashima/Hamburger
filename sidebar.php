853a2daf106a4d5c22063a07ed3d591884af8152
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2020-04-28 20:50:24 +0900

    <?php echo get_category_link(); ?>、<?php echo get_permalink();?>の追加（格ページのリンク先を追加）

--- sidebar.php
+++ sidebar.php
@@ -4,31 +4,33 @@
                     </button>
                     <ul class="list">
                         <span>Menu</span>
-                        <li>バーガー</li>
+                        <li><a href="<?php echo get_category_link(7); ?>">バーガー</a></li>
                         <ul>
-                            <li>ハンバーガー</li>
-                            <li>チーズバーガー</li>
-                            <li>テリヤキバーガー</li>
-                            <li>アボガドバーガー</li>
-                            <li>フィッシュバーガー</li>
-                            <li>ベーコンバーガー</li>
-                            <li>チキンバーガー</li>
+                            <li><a href="<?php echo get_permalink( 23 );?>">ハンバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 48 );?>">チーズバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 54 );?>">テリヤキバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 52 );?>">アボガドバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 44 );?>">フィッシュバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 50 );?>">ベーコンバーガー</a></li>
+                            <li><a href="<?php echo get_permalink( 46 );?>">チキンバーガー</a></li>
                         </ul>
-                        <li>サイド</li>
+                        <li><a href ="<?php echo get_category_link(8); ?>">サイド</a></li>
                         <ul>
-                            <li>ポテト</li>
-                            <li>サラダ</li>
-                            <li>ナゲット</li>
-                            <li>コーン</li>
+
+                            <li><a href="<?php echo get_permalink( 60 );?>">ポテト</li>
+                            <li><a href="<?php echo get_permalink( 58 );?>">サラダ</li>
+                            <li><a href="<?php echo get_permalink( 62 );?>">ナゲット</li>
+                            <li><a href="<?php echo get_permalink( 64 );?>">コーン</li>
                         </ul>
-                        <li>ドリンク</li>
+                        <li><a href="<?php echo get_category_link(9); ?>">ドリンク</a></li>
                         <ul>
-                            <li>コーラ</li>
-                            <li>ファンタ</li>
-                            <li>オレンジ</li>
-                            <li>アップル</li>
-                            <li>紅茶(Ice/Hot)</li>
-                            <li>コーヒー(Ice/Hot)</li>
+                            <li><a href="<?php echo get_permalink( 72 );?>">コーラ</li>
+                            <li><a href="<?php echo get_permalink( 23 );?>">ファンタ</li>
+                            <li><a href="<?php echo get_permalink( 68 );?>">オレンジ</li>
+                            <li><a href="<?php echo get_permalink( 76 );?>">アップル</li>
+                            <li><a href="#">紅茶(Ice/Hot)</li>
+                            <li><a href="<?php echo get_permalink( 74 );?>">コーヒー(Ice/Hot)</li>
                         </ul>
                     </ul>
                 </li>
+   
